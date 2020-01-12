c7d6dff28b295a573484f73d1a9b8791eacdae65
despire1119 <wanghaoran0218@hotmail.com>
2020-01-13 06:37:59 +0800

    s

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -92,8 +92,19 @@
         </div>
       </div>
       <div class="linkage-area">
-        <div class="map">
+        <div class="map" style="margin-bottom: 10px">
           <div id="container" style="height: 732px" />
+        </div>
+        <div class="model">
+          <div class="panel">
+            <span class="tit">案件数量分析图</span>
+            <ul class="range-opt">
+              <li class="range year">最近一年</li>
+              <li class="range mounth">最近一月</li>
+              <li class="range day">当日</li>
+            </ul>
+          </div>
+          <div id="analysis" class="cases-analysis" />
         </div>
       </div>
       <div class="right-area">
@@ -103,8 +114,62 @@
               <span class="tit">信息速查</span>
               <span class="search">
                 <input type="text">
-                <!-- <input type="text"> -->
                 <i class="tag" />
+              </span>
+            </div>
+            <div class="container">
+              <div class="img">
+                <img src alt>
+              </div>
+              <div class="row">
+                <p>
+                  <span>车牌号：</span>
+                  <i>苏AY8K32</i>
+                </p>
+                <p>
+                  <span>车籍地：</span>
+                  <i>南京</i>
+                </p>
+                <p>
+                  <span>满载质量：</span>
+                  <i>7吨</i>
+                </p>
+                <p>
+                  <span>整备质量：</span>
+                  <i>2吨</i>
+                </p>
+                <p>
+                  <span>运营证号：</span>
+                  <i>00270039</i>
+                </p>
+              </div>
+              <div class="row">
+                <p>
+                  <span>车辆类型：</span>
+                  <i>轻型</i>
+                </p>
+                <p>
+                  <span>车辆用途：</span>
+                  <i>载货</i>
+                </p>
+                <p>
+                  <span>车辆寿命：</span>
+                  <i>10年</i>
+                </p>
+                <p>
+                  <span>发动机号：</span>
+                  <i>600328</i>
+                </p>
+              </div>
+            </div>
+            <div class="bottom">
+              <span>
+                <i>累计行政处罚次数：</i>
+                <i class="fee">28次</i>
+              </span>
+              <span>
+                <i>累计行政处罚金额：</i>
+                <i class="fee">9450元</i>
               </span>
             </div>
           </div>
@@ -126,6 +191,172 @@ export default {
     return {
       adCode: 320000,
       depth: 2,
+      rankOpt: {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            crossStyle: {
+              color: '#999'
+            }
+          }
+        },
+        grid: {
+          top: '5%',
+          bottom: '15%',
+          left: '8%'
+        },
+        legend: {
+          orient: 'vertical',
+          right: '0',
+          data: ['日间', '夜间', '总量'],
+          textStyle: {
+            color: '#FFFFFF'
+          }
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: [
+              '1月',
+              '2月',
+              '3月',
+              '4月',
+              '5月',
+              '6月',
+              '7月',
+              '8月',
+              '9月',
+              '10月',
+              '11月',
+              '12月'
+            ],
+            axisPointer: {
+              type: 'shadow'
+            },
+            lineStyle: {
+              color: '#505573',
+              type: 'solid'
+            },
+            axisLabel: {
+              show: true,
+              rotate: 40,
+              textStyle: {
+                color: '#B4B8C7',
+                fontSize: '12'
+              }
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            name: '案件量',
+            lineStyle: {
+              color: '#505573',
+              type: 'solid'
+            },
+            axisLabel: {
+              show: true,
+              textStyle: {
+                color: '#B4B8C7',
+                fontSize: '12'
+              }
+            },
+            splitLine: {
+              show: true,
+              lineStyle: {
+                color: ['#505573'],
+                width: 1,
+                type: 'solid'
+              }
+            }
+          },
+          {
+            type: 'value',
+            show: false,
+            name: '案件量'
+          }
+        ],
+        series: [
+          {
+            name: '总量',
+            type: 'bar',
+            barWidth: 20,
+            data: [
+              2500,
+              4900,
+              3452,
+              2320,
+              2817,
+              2670,
+              2506,
+              2202,
+              3206,
+              2000,
+              1164,
+              1133
+            ],
+            itemStyle: {
+              normal: {
+                show: true,
+                width: 10,
+                color: new this.echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  { offset: 1, color: '#5C61FD' }, // 柱图渐变色
+                  { offset: 0, color: '#67D9FF' } // 柱图渐变色
+                ])
+              }
+            }
+          },
+          {
+            name: '日间',
+            type: 'line',
+            data: [
+              2360,
+              2359,
+              2290,
+              1264,
+              2560,
+              1771,
+              1561,
+              1212,
+              2817,
+              1818,
+              710,
+              512
+            ],
+            itemStyle: {
+              normal: {
+                color: '#03CAFE'
+              }
+            }
+          },
+          {
+            name: '夜间',
+            type: 'line',
+            yAxisIndex: 1,
+            data: [
+              140,
+              1541,
+              1162,
+              1162,
+              1056,
+              899,
+              935,
+              990,
+              389,
+              182,
+              454,
+              621
+            ],
+            itemStyle: {
+              normal: {
+                color: '#1FD68E'
+              }
+            }
+          }
+        ]
+      },
       option: {
         // visualMap: {
         //   top: 10,
@@ -313,12 +544,14 @@ export default {
     const chart4 = this.echarts.init(document.getElementById('chart4'))
     // model2
     const cases = this.echarts.init(document.getElementById('cases'))
-
+    // rank
+    const rank = this.echarts.init(document.getElementById('analysis'))
     chart1.setOption(this.options)
     chart2.setOption(this.changOption([3, 9, 52]))
     chart3.setOption(this.changOption([1, 5, 2]))
     chart4.setOption(this.changOption([2052, 2246, 3200]))
     cases.setOption(this.option)
+    rank.setOption(this.rankOpt)
   },
   methods: {
     async upload(e) {
@@ -414,16 +647,15 @@ export default {
     .map
       width: 100%
       height: 732px
-      box-shadow:0px 0px 7px 0px rgba(0, 0, 0, 0.35);
+      box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.35)
   .right-area
     width: 510px
     padding: 10
 .model
-  display: flex
   color: #fff
   background-color: #19203D
   margin-bottom: 10px
-  box-shadow:0px 0px 7px 0px rgba(0, 0, 0, 0.35);
+  box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.35)
   .panel
     flex: 1
     display: flex
@@ -454,24 +686,47 @@ export default {
       .day
         background-color: rgba(165, 53, 251, 0.43)
     .search
-      display flex
-      width 146px
-      height 28px
-      background-color #1E243D
-      border 1px solid #21C7DE
-      border-radius 14px
-      align-items center
-      padding-right 8px
+      display: flex
+      width: 146px
+      height: 28px
+      background-color: #1E243D
+      border: 1px solid #21C7DE
+      border-radius: 14px
+      align-items: center
+      padding-right: 8px
       input
-        width 118px
-        color #CCC
-        text-indent 10px
+        width: 118px
+        color: #CCC
+        text-indent: 10px
       .tag
-        display block
-        width 18px
-        height 18px
-        background-image url('~@/assets/images/sousuo@2x.png')
-        background-size contain
+        display: block
+        width: 18px
+        height: 18px
+        background-image: url('~@/assets/images/sousuo@2x.png')
+        background-size: contain
+  .container
+    display: flex
+    justify-content: space-around
+    margin-bottom: 9px
+    .img
+      width: 142px
+      height: 107px
+      background-color: #CCC
+    .row
+      p
+        margin-bottom: 5px
+  .bottom
+    display: flex
+    align-items: center
+    justify-content: space-around
+    height: 50px
+    font-size: 14px
+    font-weight: 500
+    background-color: #2172F9
+    .fee
+      font-size: 16px
+  .cases-analysis
+    height: 195px
   .case-bar
     height: 293px
   .list-content
