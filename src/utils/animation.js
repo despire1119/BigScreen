765c2bf896e5249e6408ba111eb5499b7f4f6b5a
despire1119 <wanghaoran0218@hotmail.com>
2020-01-15 07:02:13 +0800

    s

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -76,24 +76,30 @@ export class BezierCurve {
     const { q0, b } = { ...this.drawLine(origin, target) }
     this.ctx.clearRect(0, 0, this.ele.width, this.ele.height)
     // this.drawPoint(b) // 动画终点
-    this.drawPoint(origin)
-    this.ctx.beginPath()
-    this.ctx.moveTo(...origin) // 起点
-    this.ctx.quadraticCurveTo(...q0, ...b) // 贝塞尔曲线
-    this.ctx.lineWidth = 1
-    this.ctx.strokeStyle = this.color
-    this.ctx.stroke()
-    this.ctx.globalAlpha = this.opacity
+
+    if(this.percent + this.speed < 100){
+      this.drawPoint(origin)
+      this.ctx.beginPath()
+      this.ctx.moveTo(...origin) // 起点
+      this.ctx.quadraticCurveTo(...q0, ...b) // 贝塞尔曲线
+      this.ctx.lineWidth = 1
+      this.ctx.strokeStyle = this.color
+      this.ctx.stroke()
+      this.percent = (this.percent + this.speed) % 100
+    }else{
+      cancelAnimationFrame(toEnd)
+      this.ctx.globalAlpha = this.opacity
+      this.opacity-= 0.0000001
+    }
     this.ctx.closePath()
-    this.percent = (this.percent + this.speed) % 100
     // this.opacity = this.percent>90?this.opacity-this.percent/100:1
     const toEnd = requestAnimationFrame(_ => this.lineMove(origin, target))
     if (this.percent + this.speed >= 100) {
       cancelAnimationFrame(toEnd)
-      // setTimeout(() => {
-      //   this.opacity = 0
-      //   this.lineMove(origin, target)
-      // }, 1000);
+      setTimeout(() => {
+        this.opacity = 0
+        this.lineMove(origin, target)
+      }, 1000);
     }
   }
   moveOut(){
