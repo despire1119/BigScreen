f11a611f0e3d7c82b9af157d3b9fea44c518a036
despire1119 <wanghaoran0218@hotmail.com>
2020-02-28 09:35:44 +0800

    s

--- src/charts/sunData.js
+++ src/charts/sunData.js
@@ -1,7 +1,94 @@
 export const SunData = [
   {
+    name: '道路',
+    // value: 32400,
+    children: [
+      {
+        name: '运政',
+        // value: 13180,
+        children: [
+          {
+            name: '客运',
+            children: [
+              {
+                name: '班车',
+                value: 4392
+              },
+              {
+                name: '包车',
+                value: 3234
+              },
+              {
+                name: '旅游',
+                value: 3500
+              },
+              {
+                name: '出租车',
+                value: 7000
+              },
+              {
+                name: '网约车',
+                value: 4000
+              }
+            ]
+          },
+          {
+            name: '普通货物运输',
+            value: 8529
+          },
+          {
+            name: '危险品运输',
+            value: 4324
+          }
+        ]
+      },
+      {
+        name: '路政',
+        children: [
+          {
+            name: '一般程序',
+            value: 9909
+          },
+          {
+            name: '简易程序',
+            value: 3000
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: '质量',
+    children: [
+      {
+        name: '公路工程',
+        value: 8042
+      },
+      {
+        name: '铁路工程',
+        value: 8012
+      },
+      {
+        name: '水运工程',
+        value: 8953
+      },
+      {
+        name: '建设市场',
+        value: 8302
+      }
+    ]
+  },
+  {
     name: '水上',
     children: [
+      {
+        name: '航政',
+        value: 5020
+      },
+      {
+        name: '运政',
+        value: 5990
+      },
       {
         name: '海事',
         children: [
@@ -32,110 +119,19 @@ export const SunData = [
         ]
       },
       {
-        name: '运政',
+        name: '港政',
         children: [
           {
-            name: '客运',
+            name: '从业人员',
             value: 6000
           },
           {
-            name: '普通货运',
+            name: '生产过程',
             value: 4399
           },
           {
-            name: '危险货运',
+            name: '设备设施',
             value: 3999
-          },
-          {
-            name: '维修陪驾',
-            value: 3423
-          }
-        ]
-      },
-      {
-        name: '航政',
-        value: 5020
-      },
-      {
-        name: '港政',
-        value: 5990
-      }
-    ]
-  },
-  {
-    name: '质量',
-    children: [
-      {
-        name: '公路工程',
-        value: 8042
-      },
-      {
-        name: '铁路工程',
-        value: 8012
-      },
-      {
-        name: '水运工程',
-        value: 8953
-      },
-      {
-        name: '建设市场',
-        value: 8302
-      }
-    ]
-  },
-  {
-    name: '道路',
-    // value: 32400,
-    children: [
-      {
-        name: '运政',
-        // value: 13180,
-        children: [
-          {
-            name: '客运',
-            children: [
-              {
-                name: '班车',
-                value: 4392
-              },
-              {
-                name: '包车',
-                value: 3234
-              },
-              {
-                name: '巡游',
-                value: 3500
-              },
-              {
-                name: '出租车',
-                value: 7000
-              },
-              {
-                name: '网约车',
-                value: 4000
-              }
-            ]
-          },
-          {
-            name: '普通货运',
-            value: 8529
-          },
-          {
-            name: '危险货运',
-            value: 4324
-          }
-        ]
-      },
-      {
-        name: '路政',
-        children: [
-          {
-            name: '一般程序',
-            value: 9909
-          },
-          {
-            name: '简易程序',
-            value: 3000
           }
         ]
       }
